f1139f1b30e47c4941dc1b06143f51a80a3f9fab
KaiWangCanada <kaiwangcanada@gmail.com>
2015-09-10 15:23:00 -0400

    SDKShare

--- app/src/main/java/kai/onceuponatime/MainActivity.java
+++ app/src/main/java/kai/onceuponatime/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class MainActivity extends Activity {
 
@@ -14,4 +18,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void click(View view) {
+        showShare();
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
+    }
 }
